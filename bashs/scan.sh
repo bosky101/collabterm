592d2149ef174d0d3ac45f1d51236e876d62620f
Bhasker V Kode <bosky101@gmail.com>
2012-05-05 17:03:04 +0530

    tested with another terminal for the first time. scrapping scan via nmap, using nodejs itself

--- bashs/scan.sh
+++ bashs/scan.sh
@@ -1,4 +1,15 @@
 #/bin/bash
-for i in 100 .. 110;do
-	alive = `sudo nmap -sS -O -p 8765 192.168.10.$i |grep 8765`
+port=8765
+for i in {100..110}
+do
+	ip=192.168.10.$i
+	cmd="sudo nmap -v4 -sS -n -p $port $ip |grep $port"
+	echo "Searching $ip for port $port, running cmd: $cmd"
+	alive=`$cmd`
+	echo "got alive as $alive"
+	alivelen=${#alive}
+	if [ $alivelen -ge 1]
+	then
+		echo $i is alive with $alive
+	fi
 done
--- node/server.js
+++ node/server.js
@@ -1,7 +1,101 @@
-var express = require('express');
+var express = require('express'), colors = require('colors'), faye = require('faye'), argv = require('optimist').argv, _ = require('underscore');
+var util = require('util'), exec = require('child_process').exec;
+var inspect = require('inspect');
+var cli = require('readline').createInterface(process.stdin, process.stdout, null);
+DEBUG=1;
 
-var app = express.createServer();
+var _log = function(){
+    if(DEBUG){
+	console.log.apply(console.log,arguments);
+    }
+};
 
-app.get('*', function(req,res){ res.send('hello world');});
+var CollabTerm = function(){
+    var self = this;
+};
 
-app.listen(8000);
+CollabTerm.networkReady = function(_scope){
+    return function(error, stdout, stderr) { 
+	var _this = _scope;
+	_this.port = (argv.port) ? argv.port : "8765";
+	_this.host = (argv.host) ? argv.host : stdout.replace('\n','');
+	_log('network ready with ',_this.host.length+":"+_this.port);
+	_this.setupFaye();
+    };
+};
+CollabTerm.prototype.p = function(str){
+    if(!str){
+	process.stdout.write('> '.green);
+    }else{
+	process.stdout.write(str);
+    }
+};
+
+var client;
+
+CollabTerm.prototype.setupFaye = function(){
+    var _this = this;
+
+    global.app = express.createServer();
+    _log('listen on '+_this.host+':'+_this.port);
+    app.listen(_this.port,_this.host);
+
+    var bayeux = new faye.NodeAdapter({mount: '/faye', timeout: 5});
+    var auth = require('./faye-exts/auth.js');
+    bayeux.attach(app);
+  
+    _log('Faye Listening on ', _this.host + ':'+_this.port+'/faye');
+
+    var stdin = process.openStdin(); 
+    require('tty').setRawMode(true);    
+    
+    var room = '/'+ ( (argv.room)? argv.room : 'global');
+    var user = (argv.user) ? argv.user : 'Guest';
+    cli.on('line', function(chunk){	
+	var _published = bayeux.getClient().publish(room, {typed:chunk,user:user});
+	console.log('send '+chunk.red);
+    });
+
+    var _joined = bayeux.getClient().subscribe(room,function(message){
+	//process.stdout.write(message.typed.red);
+	if(message.typed){
+	    exec(message.typed, function( _er1,_stdout1,_stderr1 ){
+		_this.p(message.typed + ' typed gave...\n' + _stdout1);
+		if(message.user == user){
+		    return;
+		}
+		bayeux.getClient().publish(room,{output:_stdout1,user:message.user});
+	    });
+	}
+    });
+
+    bayeux.getClient().subscribe('/discovery/*', function(message){
+	var reply_to = message.channel+'/reply';
+	console.log('/discovery/* got from '+message.channel,' reply to '+reply_to);
+	bayeux.getClient().publish(reply_to, {channel:message.channel,host:_this.host,port:_this.port,status:200});
+    });
+
+    _this.p();
+
+    if(DEBUG){
+	bayeux.bind('handshake', function(clientId) {
+	    console.log('[  HANDSHAKE ] ' + clientId);
+	});
+	
+	bayeux.bind('subscribe', function(clientId, channel) {
+	    console.log('[  SUBSCRIBE] ' + clientId + ' -> ' + channel);    
+	});
+	
+	bayeux.bind('publish', function(clientId, channel, data){
+	    console.log('[ PUBLISH] to ' + channel, 'with data ',data);
+	});
+	
+	bayeux.bind('unsubscribe', function(clientId, channel) {
+	    console.log('[ UNSUBSCRIBE] ' + clientId + ' -> ' + channel);
+	});
+    }
+
+};
+
+var CT  = new CollabTerm();
+exec("(/usr/sbin/arp $(hostname) | awk -F'[()]' '{print $2}')", CollabTerm.networkReady(CT));
